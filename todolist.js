fab519189a2736616a70b6bb8eac6fce5073cd48
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-02-03 17:22:09 +0530

    cookie changes

--- todolist.js
+++ todolist.js
@@ -63,23 +63,4 @@ function newElement() {
       div.style.display = "none";
     }
   }
-  
-  //cookie to store the no. of clicks of button
-  var cookie = document.cookie;
-  if(cookie!= null && cookie!=undefined){
-    if(cookie.length>0){
-      var cookies = cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) =>({ ...accumulator, [key.trim()]: decodeURIComponent(value) }),{});
-      var seenCount= cookies.click;
-      //Not first time click!!
-      seenCount++;
-      //Then save the cookie again
-      document.cookie = "click="+seenCount+";";
-    }
-  }
-  else{
-    //Treat as first time visit
-    document.cookie = "click=1;";
-  }
-  document.getElementById("clicks").innerHTML = "button clicked "+seenCount+" times."
-  //alert("button clicked "+seenCount+" times.");
 }
