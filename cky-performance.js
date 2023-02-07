1ca41e13b336fcaac32f1456fcec288c493b61c8
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-02-07 15:18:40 +0530

    Update cky-performance.js

--- cky-performance.js
+++ cky-performance.js
@@ -1,23 +1,21 @@
 var cookie = document.cookie;
-    if(cookie!= null && cookie!=undefined){
-      if(cookie.length>0){
-        var cookies = cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) =>({ ...accumulator, [key.trim()]: decodeURIComponent(value) }),{});
-        var seenCount= cookies.visit;
-
-        //Not first time visit!!
-        seenCount++;
-        //Then save the cookie again
-        document.cookie = "visit="+seenCount+";";
-      }
+  if(cookie!= null && cookie!=undefined){
+    if(cookie.length>0){
+      var cookies = cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) =>({ ...accumulator, [key.trim()]: decodeURIComponent(value) }),{});
+      var seenCount= cookies.visit;
+      //Not first time visit!!
+      seenCount++;
+      //Then save the cookie again
+      document.cookie = "visit="+seenCount+";";
     }
-    else{
+  }
+  else{
     //Treat as first time visit
-      document.cookie = "visit=1;";
-    }
-    document.cookie = "click=0;";
-    //document.getElementsByClassName('demo').innerHTML='Visitinf for'+seenCount+'time';
-    //document.getElementById("visitno").innerHTML = "visited website "+seenCount+" times."
-
+    document.cookie = "visit=1;";
+  }
+  document.cookie = "click=0;";
+  //document.getElementsByClassName('demo').innerHTML='Visitinf for'+seenCount+'time';
+  //document.getElementById("visitno").innerHTML = "visited website "+seenCount+" times."
 
 //cookie to store the no. of clicks of button
 var cookie = document.cookie;
